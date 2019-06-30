e4ad51758bca235b60c7e158b10bbf0f521fa5e1
kamino <lishichenjs@163.com>
2019-06-30 22:34:23 +0800

    初始版本

--- resources/js/components/AuthComponent.vue
+++ resources/js/components/AuthComponent.vue
@@ -1,20 +1,36 @@
 <template>
-<el-input v-model="input" placeholder="请输入内容"></el-input>
+  <el-button
+    type="primary"
+    v-loading.fullscreen.lock="fullscreenLoading">
+    指令方式
+  </el-button>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      input: ''
+  export default {
+    data() {
+      return {
+        fullscreenLoading: false
+      }
+    },
+    methods: {
+      openFullScreen() {
+        this.fullscreenLoading = true;
+        setTimeout(() => {
+          this.fullscreenLoading = false;
+        }, 2000);
+      },
+      openFullScreen2() {
+        const loading = this.$loading({
+          lock: true,
+          text: 'Loading',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        });
+        setTimeout(() => {
+          loading.close();
+        }, 2000);
+      }
     }
   }
-};
 </script>
-
-<style>
-.hello {
-  font-size: 2em;
-  color: green;
-}
-</style>
--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>请输入密码</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
-    <div id="app">
-    </div>
-    
-<script src="{{ mix('js/auth.js') }}"></script>
+    <form action="/" method="post">
+        @csrf
+        <input type="text" name="key"><br>
+        <input type="submit">
+    </form>
 </body>
+
 </html>
